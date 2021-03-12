48b244d29886cc98cab215c49f22b1cc40085f0f
luizacostab <>
2021-03-11 22:40:33 -0300

    Configuração da Framework, criação da nova pagina inicial e sua controller

---
+++ public/application/views/home.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Site PW3</title>
+
+	
+</head>
+<body>
+	<h1>Seja bem vindo</h1>
+
+	
+
+</body>
+</html>
