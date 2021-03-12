e38120d2449d34ba79b14b0347a15941f4bebef0
luizacostab <>
2021-03-11 23:23:37 -0300

    Iniciando o layout

--- public/application/views/home.php
+++ public/application/views/home.php
@@ -6,11 +6,41 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>Site PW3</title>
-
+	<link rel="stylesheet" href="<?=  base_url() ?>assets/reset.css">
+	<link rel="stylesheet" href="<?=  base_url() ?>assets/principal.css">
+	<link rel="stylesheet" href="<?=  base_url() ?>assets/css/bootstrap.min.css">
 	
 </head>
 <body>
-	<h1>Seja bem vindo</h1>
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+
+			<div class="jumbotron">
+  <h1 class="display-4">Seja bem vindo!</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+			<ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Pagina 1</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Pagina 2</a>
+  </li>
+</ul>
+
+			</div>
+		</div>
+
+		
+	</div>
+
 
 	
 
